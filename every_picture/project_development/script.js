1b28d992eb9e0f03ac7297d0f664d7d12527fc22
Kenny_Yu <kennylgm45@gmail.com>
2026-02-18 21:25:01 -0800

    first draft of the every picture project

---
+++ every_picture/project_development/script.js
@@ -0,0 +1,19 @@
+(function(){
+    "use strict";
+    console.log("running js");
+
+    // const one = document.querySelector("#one");
+    // const two = document.querySelector("#two");
+    // const three = document.querySelector("#three");
+    // const four = document.querySelector("#four");
+    // const five = document.querySelector("#five");
+    // const six = document.querySelector("#six");
+
+    // one.addEventListener("mouseover", function(){
+    //     one.classList.add("expand");
+    // })
+    // one.addEventListener("mouseout", function(){
+    //     one.classList.remove("expand");
+    // })
+
+})();
